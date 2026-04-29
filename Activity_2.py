0c0683ed7b7b5a900353bfce75383771849b743a
ronakpatel29313 <ronak.patel29313@gmail.com>
2026-04-29 19:45:14 +0100

    activity 2

---
+++ Activity_2.py
@@ -0,0 +1,8 @@
+date = "25"
+month = "october"
+year = "2000"
+print("I was born on ", date,"th of ", month ,year)
+print("I was born on"+ date,"th of"+month+" "+year)
+
+height = 1.75
+print("I am", height, "meters tall")
